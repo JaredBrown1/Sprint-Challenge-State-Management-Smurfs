9db92b6c6a56e25e09d1729b0e2dc5c412356959
Jared Brown <jaredbrown1@hotmail.com>
2020-04-18 09:43:41 -0700

    restart for flex/ actions created

--- smurfs/src/actions/actions.js
+++ smurfs/src/actions/actions.js
@@ -4,31 +4,31 @@ export const FETCH_DATA = "FETCH_DATA";
 export const UPDATE_SMURF = "UPDATE_SMURF";
 export const SET_ERROR = "SET_ERROR";
 
-export const getData = () => dispatch => {
+export const getData = () => (dispatch) => {
   dispatch({ type: FETCH_DATA });
   axios
     .get("http://localhost:3333/smurfs")
-    .then(res => {
+    .then((res) => {
       console.log("from .then", res);
       dispatchEvent({ type: UPDATE_SMURF, payload: res.data });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log("error fetching data from api. err: ", err);
       dispatch({ type: SET_ERROR, payload: err });
     });
 };
 
-export const addSmurf = smurf => dispatch => {
+export const addSmurf = (smurf) => (dispatch) => {
   console.log(smurf);
   axios
     .post("http://localhost:3333/smurfs", {
-      ...smurf
+      ...smurf,
     })
-    .then(res => dispatch({ type: UPDATE_SMURF, payload: res.data }))
-    .catch(err => dispatch({ type: SET_ERROR, payload: err }));
+    .then((res) => dispatch({ type: UPDATE_SMURF, payload: res.data }))
+    .catch((err) => dispatch({ type: SET_ERROR, payload: err }));
   return {
     name: "",
     height: "",
-    age: ""
+    age: "",
   };
 };
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import SmurfForm from "./smurfForm";
-import SmurfList from "./smurfList";
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <SmurfList />
+        <h1>SMURFS! 2.0 W/ Redux</h1>
+        <div>Welcome to your state management version of Smurfs!</div>
+        <div>Start inside of your `src/index.js` file!</div>
+        <div>Have fun!</div>
       </div>
     );
   }
